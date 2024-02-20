965b8ab0831ca09c88444df94d6d7fed25f202c7
Greg Lixandrão <contato@greglixandrao.com>
2024-02-20 13:49:30 -0300

    feat: exception handler, 404 product not found

---
+++ src/main/java/com/example/crud/infra/RequestsExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.crud.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){
+//        var response = new ExceptionDTO();
+        return ResponseEntity.badRequest().body("Dado não encontrado");
+    }
+}
